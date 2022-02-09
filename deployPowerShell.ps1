8b86515671e2c9c37903fc7e0152d989c73818e8
Mohsin Yasin <mohsinbu@gmail.com>
2022-02-08 22:14:13 -0500

    Update deployPowerShell.ps1
    VM extension will only process one command at a time if you get a 409 "Run command extension execution is in progress. Please wait for completion before invoking a run command" you must wait and retry.  This might need additional time or possibly another retry

--- deployPowerShell.ps1
+++ deployPowerShell.ps1
@@ -28,10 +28,56 @@ Start-Process "$env:Temp\PowerShell-7.1.3-win-x64.msi" -ArgumentList "/quiet /no
 
     (Get-Content $env:Temp\script.ps1 -Raw) -replace "VALUE", $sasUri | Set-Content $env:Temp\script.ps1 -Force
 
-    Invoke-AzVMRunCommand -ResourceGroupName $vmResourceGroupName -VMName $vmName -ScriptPath $env:Temp\script.ps1 -CommandId 'RunPowerShellScript' -Verbose
+ Write-verbose "Trying command:  Invoke-AzVMRunCommand" 
+           $StartTime = Get-Date
+           ## Attempting to run the Invoke-AzVMRunCommand against a VM that is not available will cause a 90 minute timeout
+           $VMstatus = Get-AzVM -ResourceGroupName $vmResourceGroupName -Name $vmName -Status
+           if ($VMstatus) 
+             {
+                if ($VMstatus.VMAgent.Statuses[0].Code -eq "ProvisioningState/succeeded") 
+                {
+
+                  "Running Invoke-AzVMRunCommand -ResourceGroupName $vmResourceGroupName -Name $vmName ..."
+                  $Results = Invoke-AzVMRunCommand -ResourceGroupName $vmResourceGroupName -VMName $vmName -ScriptPath $env:Temp\script.ps1 -CommandId 'RunPowerShellScript' -Verbose
+                  "Result: " + $Results.Value.message
+                }
+                else 
+                {
+                  Write-verbose "$vmName is not ready for Invoke-AzVMRunCommad"
+                }
+             }
+           $EndTime = Get-Date
+           If ($Results) {
+               "    $VMName Finished    TotalSeconds: " + $EndTime.Subtract($StartTime).TotalSeconds + "  with "  + $Results.Status
+              }  
+           else  ## If $Result is NULL
+           {   Write-verbose "Retrying command:  Invoke-AzVMRunCommand"   ## Note this will only show up if the runbook "Logging and Tracing" is enabled with Verbose
+               Start-sleep -seconds 180  # VM extension will only process one command at a time, if you get a 
+                                        # 409 "Run command extension execution is in progress. Please wait for completion before invoking a run command" 
+                                        # you must wait and retry.  This might need additional time or possibly another retry.
+               $StartTime = Get-Date
+               $VMstatus = Get-AzVM -ResourceGroupName $VMRG -Name $VMName -Status
+               if ($VMstatus) 
+                 {
+                   if ($VMstatus.VMAgent.Statuses[0].Code -eq "ProvisioningState/succeeded") 
+                       {
+                         "Running Invoke-AzVMRunCommand -ResourceGroupName $vmResourceGroupName -Name $vmName ..."
+                         $Results = Invoke-AzVMRunCommand -ResourceGroupName $vmResourceGroupName -VMName $vmName -ScriptPath $env:Temp\script.ps1 -CommandId 'RunPowerShellScript' -Verbose
+                         "Result2: " + $Results.Value.message
+                       }
+                 }
+               else 
+                 {
+                   Write-verbose "$VMName is not ready for Invoke-AzVMRunCommad"
+                 }
+               if (!$Results) {$ErrMessage = $error[0].Exception.Message}  ## If $Result is NULL
+               $EndTime = Get-Date
+               If ($Results) {"    $VMName Finished    TotalSeconds: " + $EndTime.Subtract($StartTime).TotalSeconds + "  with "  + $Results.Status}
+               else {    "    $VMName  Error: " + $ErrMessage}
+           } 
+
 }
 else {
     Write-Output "Event subject does not match microsoft.compute"
 }
-
 
